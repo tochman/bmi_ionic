2de487bfc705fb5a534a43aadc5cbbfa1bc7dd2b
tochman <thomas@nocebo.se>
2016-03-24 16:12:54 +0100

    added basic calculation with hardcoded data

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,6 +4,10 @@ angular.module('starter.controllers', [])
 
   })
 
-  .controller('BmiController', function() {
+  .controller('BmiController', function($scope) {
+    var person = new Person({weight: 80, height: 186});
+    person.calculate_bmi_met();
+    $scope.person = person;
+
 
   });
---
+++ www/js/person.js
@@ -0,0 +1,15 @@
+
+function Person(attr) {
+  this.weight = attr.weight;
+  this.height = attr.height;
+};
+
+Person.prototype.calculate_bmi_met = function() {
+  calculator = new BMICalculator();
+  calculator.metric_bmi(this);
+};
+
+Person.prototype.calculate_bmi_imp = function() {
+  calculator = new BMICalculator();
+  calculator.imperial_bmi(this);
+};
