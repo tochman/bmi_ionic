38b8471ffed1a8da1693c560c78b416e0fcaac26
Mazen <mmeezzoo26@me.com>
2016-03-29 12:57:17 +0200

    fix for loading images to ionic upload

--- www/js/bmi_calculator.js
+++ www/js/bmi_calculator.js
@@ -23,22 +23,22 @@ BMICalculator.prototype.imperial_bmi = function(obj) {
 function setBMIMessage (obj, value){
   if (obj.bmiValue < 18.5) {
     obj.bmiMessage = "Underweight"
-    obj.bmiPicture = "../img/xs.jpeg"
+    obj.bmiPicture = "img/xs.jpeg"
     obj.bmiBrief = "Skinny is not the new sexy anymore my friend!, maybe more exercise and enriched diet will take you to the next level"
   }
   if (obj.bmiValue > 18.5 && obj.bmiValue < 25) {
     obj.bmiMessage = "Normal"
-    obj.bmiPicture = "../img/m.png"
+    obj.bmiPicture = "img/m.png"
     obj.bmiBrief = "Normal, does not means perfect, some finetunning here and there ... but you almost there"
   }
   if (obj.bmiValue > 25 && obj.bmiValue < 30) {
     obj.bmiMessage = "Overweight"
-    obj.bmiPicture = "../img/l.png"
+    obj.bmiPicture = "img/l.png"
     obj.bmiBrief = "Everyone loves the curvy style, lets make sure its in the correct places, some finetunning to be good on the long run"
   }
   if (obj.bmiValue > 30) {
     obj.bmiMessage = "Obese"
-    obj.bmiPicture = "../img/xl.jpg"
+    obj.bmiPicture = "img/xl.jpg"
     obj.bmiBrief = "A bit too much of love-hangers, health first and then all to follow...but nothing impossible "
   }
 }
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -28,25 +28,6 @@ angular.module('bmi_calculator.controllers', [])
 
 })
 
-//
-// .controller('BmiController', function ($scope) {
-//
-//   $scope.data = {};
-//   $scope.data.measurement = '';
-//
-//   $scope.calculateBMI = ('data.measurement', function () {
-//     var person = new Person({weight: $scope.data.weight, height: $scope.data.height});
-//
-//     if($scope.data.measurement== "metric")
-//       person.calculate_bmi_met();
-//     $scope.person = person;
-//     else
-//     person.calculate_bmi_imp();
-//     $scope.person = person;
-//   });
-//
-// })
-
 .controller('CoverController', function($scope, $state) {
   $scope.data = {};
   $scope.measurment_option = [{
